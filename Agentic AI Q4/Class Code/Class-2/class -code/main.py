9820dafc76dd1647ff38fa7f7ed41a9dc8837553
AsmaAkbar88 <164855203+AsmaAkbar88@users.noreply.github.com>
2025-07-01 22:42:43 +0500

    Sir-Hamza-class-2-code-simple-agent

---
+++ Agentic AI Q4/Class Code/Class-2/class -code/main.py
@@ -0,0 +1,51 @@
+
+import os
+from dotenv import load_dotenv
+from agents import Agent, Runner, OpenAIChatCompletionsModel , AsyncOpenAI, RunConfig
+import asyncio
+
+async def main():
+    load_dotenv()
+
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY") 
+    MODEL_NAME = "gemini-2.0-flash"
+
+    if not GEMINI_API_KEY:
+        raise RuntimeError("KEY NOT FOUND")
+
+    external_client = AsyncOpenAI(
+        api_key = GEMINI_API_KEY ,
+        base_url = "https://generativelanguage.googleapis.com/v1beta/openai/"
+    )
+
+    model = OpenAIChatCompletionsModel(
+        model= MODEL_NAME ,
+        openai_client=external_client
+    )
+
+
+    config = RunConfig(
+        model=model,
+        model_provider = external_client,
+        tracing_disabled = True
+    )
+
+    agent = Agent(
+        name = "assistant",
+        instructions = "you are a helpful assistant",
+        model = model
+    )
+
+    result = await Runner.run(
+        agent,
+        "tell me about piaic.",
+        run_config = config
+    )
+
+    print(result.final_output)
+
+
+
+if __name__ == "__main__":
+    asyncio.run( main())
+   
