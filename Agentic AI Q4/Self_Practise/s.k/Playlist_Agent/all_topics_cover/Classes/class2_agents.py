dfa621301e0d0436a2efa121e45c106ba3175245
AsmaAkbar88 <164855203+AsmaAkbar88@users.noreply.github.com>
2025-08-29 16:20:02 +0500

    Add_S.K_Class2_Agents

--- Agentic AI Q4/Self_Practise/s.k/Playlist_Agent/all_topics_cover/Classes/class2_agents.py
+++ Agentic AI Q4/Self_Practise/s.k/Playlist_Agent/all_topics_cover/Classes/class2_agents.py
@@ -1,39 +1,53 @@
-from agents import OpenAIChatCompletionsModel, AsyncOpenAI, RunConfig,Runner, Agent
+# 🔹  ============ Required Imports ============ 
+from agents import OpenAIChatCompletionsModel, AsyncOpenAI, RunConfig, Runner, Agent
 import os
 from dotenv import load_dotenv
 
 
-
+# 🔹  ============ Load Environment Variables ============ 
 load_dotenv()
 
+
+# 🔹 ============  Gemini API Key ============ 
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
 
+# 🔹  ============ Provider Setup (AsyncOpenAI) ============ 
 provider = AsyncOpenAI(
     api_key=gemini_api_key,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 
+
+# 🔹  ============ Model Setup ============ 
 model = OpenAIChatCompletionsModel(
     model="gemini-2.0-flash",
     openai_client=provider
 )
 
+
+# 🔹  ============ Run Configuration ============ 
 config = RunConfig(
     model=model,
     model_provider=provider,
     tracing_disabled=True
 )
 
+
+# 🔹  ============ Agent Definition ============ 
 agent_one = Agent(
     name="Frontend Expert",
     instructions="you are a forntend expert",
 )
 
+
+# 🔹  ============ Run Agent (Sync Mode) ============ 
 result = Runner.run_sync(
     agent_one,
     input="Helo how are you",
     run_config=config
 )
 
+
+# 🔹  ============ Print Final Output ============ 
 print(result.final_output)
