5a06585bd9792465a67dbd32388de97af06aa06e
AsmaAkbar88 <164855203+AsmaAkbar88@users.noreply.github.com>
2025-09-04 14:30:22 +0500

    Add_S.K_Class7_Streamed

---
+++ Agentic AI Q4/Self_Practise/s.k/Playlist_Agent/all_topics_cover/Classes/class7_streamed.py
@@ -0,0 +1,64 @@
+# 🔹  ============ Required Imports ============ 
+import asyncio
+from agents import OpenAIChatCompletionsModel, AsyncOpenAI, RunConfig, Runner, Agent
+import os
+from dotenv import load_dotenv
+from openai.types.responses import ResponseTextDeltaEvent
+
+# 🔹  ============ Load Environment Variables ============ 
+load_dotenv()
+
+
+# 🔹 ============  Gemini API Key ============ 
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+
+# 🔹  ============ Provider Setup (AsyncOpenAI) ============ 
+provider = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+
+# 🔹  ============ Model Setup ============ 
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=provider
+)
+
+
+# 🔹  ============ Run Configuration ============ 
+config = RunConfig(
+    model=model,
+    model_provider=provider,
+    tracing_disabled=True
+)
+
+
+# 🔹  ============ Agent Definition ============ 
+async def main():
+    agent_one = Agent(
+        name="Frontend Expert",
+        instructions="you are a forntend expert",
+)
+
+
+# 🔹  ============ Run Agent (Sync Mode) ============ 
+    result = Runner.run_streamed(
+        agent_one,
+        input="Helo how are you",
+        run_config=config
+)
+
+
+
+ # 🔹  ========== Streamed Events ==========
+    async for event in result.stream_events():
+        if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+            print(  event.data.delta , end="", flush=True)
+
+
+
+# 🔹  ============ Run Main ============
+if __name__ == "__main__":
+    asyncio.run(main())
